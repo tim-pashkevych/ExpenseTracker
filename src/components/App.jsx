a77fed0893073446f8a05c1e78165ff40bcf253f
ordila <ordila99@gmail.com>
2024-02-08 16:16:26 +0200

    add initial libraries, reset styles,initial routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "../components";
 function App() {
-  return <h1>Ready to start?</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element />
+        <Route path="sign_in" element />
+        <Route path="sign_up" element />
+        <Route path="transactions/:transactionsType" />
+        <Route path="transactions/history/:transactionsType" />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
