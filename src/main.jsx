83e9c2a1289f6daa8db207a2c6c1ff497c4c4f3e
Kovtunets Mykola <kovtunets73@gmail.com>
2024-02-09 12:29:26 +0100

    persisted

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./components/App.jsx"
-import "modern-normalize/modern-normalize.css"
-import "./index.css"
+import { PersistGate } from "redux-persist/integration/react"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import { store } from "./redux"
+
+import App from "./components/App.jsx"
+
+import { persistor, store } from "./redux"
+import "modern-normalize/modern-normalize.css"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit"
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
 import { authReducer } from "./auth/slice"
 import { userReducer } from "./user/slice"
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["sid", "accessToken", "refreshToken"],
+}
+
+const persistedAuth = persistReducer(persistConfig, authReducer)
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuth,
     user: userReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: import.meta.env.NODE_ENV !== "production",
 })
+
+export const persistor = persistStore(store)
