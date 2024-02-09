50ab72c11d93c9f1fb604b5cf189ed018d9c69b4
ordila <ordila99@gmail.com>
2024-02-09 22:14:41 +0200

    add aliases and fix loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,32 @@
 import { Outlet } from "react-router-dom";
 import { Container } from "../index";
-
+import Icon from "icons/Logo.svg?react";
+import classNames from "classnames";
+import styles from "./Layout.module.css";
 export const Layout = () => {
+  const isLoggedIn = true;
+
+  const classNamess = classNames({
+    [styles.red]: isLoggedIn,
+  });
+
+  // styles[`host-btn-${size}`],
   return (
     <Container>
       {/* Header  */}
+      {isLoggedIn && (
+        <p className={classNamess}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel obcaecati
+          quidem, voluptas iusto delectus libero commodi nobis reiciendis,
+          aliquam error consequuntur, quo aperiam ut sequi facere. Similique
+          accusamus nam earum. Assumenda harum reprehenderit officia magnam
+          fugiat deleniti veritatis dolore illum, consequatur, eveniet ad soluta
+          dolor ea. Nihil odio, exercitationem similique itaque recusandae iure
+          voluptatibus rem, reprehenderit veritatis cupiditate voluptatum
+          dolore.
+        </p>
+      )}
+      <Icon />
       <Outlet />
     </Container>
   );
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,15 @@
 import { InfinitySpin } from "react-loader-spinner";
-import { LoaderWrapper } from "../style/Loader.style";
+import styles from "./Loader.module.css";
 
 export const Loader = () => {
   return (
-    <LoaderWrapper>
+    <div className={styles.loader_wrapper}>
       <InfinitySpin
         visible={true}
         width="300"
         color="#4fa94d"
         ariaLabel="infinity-spin-loading"
       />
-    </LoaderWrapper>
+    </div>
   );
 };
