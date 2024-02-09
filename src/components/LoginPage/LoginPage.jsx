51bc997e2e49adc03bbf1a94e772eb6b6f95dace
Annazubakha <korepovaanna@gmail.com>
2024-02-09 22:51:02 +0100

    authform
    add styles-mobile and logic of props  for authform

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,17 @@
 import { AuthForm } from "../index";
 import styles from "./LoginPage.module.css";
 export const LoginPage = () => {
+  const formData = [
+    { name: "email", type: "text", placeholder: "Email" },
+    { name: "password", type: "password", placeholder: "Password" },
+  ];
+  const onSumbit = (data) => {};
+  const navigation = {
+    text: "Don't have an account?",
+    textLink: "Sign Up",
+    route: "/register",
+  };
+  const divWrapper = { marginBottom: "142px" };
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Sign In</h1>
@@ -8,7 +19,14 @@ export const LoginPage = () => {
         Welcome back to effortless expense tracking! Your financial dashboard
         awaits.
       </p>
-      <AuthForm />
+      <AuthForm
+        divWrapperStyles={divWrapper}
+        formData={formData}
+        buttonText="Sign In"
+        onSumbit={onSumbit}
+        navigation={navigation}
+        // validation={validationSchema}
+      />
     </div>
   );
 };
