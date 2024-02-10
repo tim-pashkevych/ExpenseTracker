e24738d2abf69c9512a3d4410a3c51f30f6e8c38
ordila <ordila99@gmail.com>
2024-02-10 11:56:14 +0200

    add private/public routes + refreshing logic

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+// import { selectIsLoggedIn } from "@/redux/auth/slice";
+// import { useSelector } from "react-redux";
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
+  // const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoggedIn = false;
+  const location = useLocation();
+
+  return !isLoggedIn ? (
+    <Navigate state={{ from: location }} to={redirectTo} />
+  ) : (
+    Component
+  );
+};
