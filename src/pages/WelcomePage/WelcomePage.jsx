6f6bb84652fbe1d2c8068ce5acb7201e5a8d90bc
Annazubakha <korepovaanna@gmail.com>
2024-02-11 01:02:08 +0100

    added styles main page
    added styles all welcome page, register login for tablet and mobile

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,23 +3,27 @@ import styles from "./WelcomePage.module.css";
 
 export const WelcomePage = () => {
   return (
-    <div>
+    <div className={styles.wrapperWelcomePageDesktop}>
       <div className={styles.wrapperWelcomePage}>
-        <p className={styles.description}>Expense log</p>
-        <h1 className={styles.mainTitle}>
-          Manage Your <span className={styles.mainTitleSpan}>Finances</span>{" "}
-          Masterfully!
-        </h1>
-        <p className={styles.mainDescription}>
-          ExpenseTracker effortlessly empowers you to take control of your
-          finances! With intuitive features, it simplifies the process of
-          tracking and managing expenses, allowing for a stress-free mastery
-          over your financial world.
-        </p>
+        <div className={styles.wrapper}>
+          <p className={styles.description}>Expense log</p>
+          <h1 className={styles.mainTitle}>
+            Manage Your <span className={styles.mainTitleSpan}>Finances</span>{" "}
+            Masterfully!
+          </h1>
+          <p className={styles.mainDescription}>
+            ExpenseTracker effortlessly empowers you to take control of your
+            finances! With intuitive features, it simplifies the process of
+            tracking and managing expenses, allowing for a stress-free mastery
+            over your financial world.
+          </p>
+        </div>
+        <AuthNav />
       </div>
-      <AuthNav />
-      {/* <BgImageWrapper /> */}
-      <AllUsersTab />
+      <div className={styles.bgImage}>
+        <BgImageWrapper className={styles.bgImage} />
+        <AllUsersTab />
+      </div>
     </div>
   );
 };
