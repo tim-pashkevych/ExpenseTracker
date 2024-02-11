3ed1295d0ad553f90aefde5b9a4cf6e0ccc2c13e
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-02-11 16:29:24 +0200

    Feature: Transactions History: basic implementation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { Container, HeaderComponent } from "../index";
+import { Outlet } from "react-router-dom"
+import { Container, HeaderComponent, Loader } from "../index"
+import { Suspense } from "react"
 
 export const Layout = () => {
   return (
     <Container>
       <HeaderComponent />
-      <Outlet />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </Container>
-  );
-};
+  )
+}
