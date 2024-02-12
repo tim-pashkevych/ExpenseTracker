d510ef1f603cafe0fc3e5a055af168b8cb617cbf
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-02-12 03:34:16 +0200

    Transactions History: styles for tablet and desktop

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,22 +1,26 @@
 import { Suspense } from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 
 import { BgImageWrapper, Container, HeaderComponent, Loader } from "../index"
 
 import { useSelector } from "react-redux"
 import { selectIsLoading } from "@/redux/auth/slice"
+import { ROUTES } from "./../../constants"
+
+const { HOME, SIGN_IN, SIGN_UP } = ROUTES
 
 export const Layout = () => {
+  const { pathname } = useLocation()
   const isLoading = useSelector(selectIsLoading)
 
   return (
     <Container>
       <HeaderComponent />
 
-
       {isLoading ? <Loader /> : <Outlet />}
-      <BgImageWrapper />
-
+      {pathname === HOME || pathname === SIGN_IN || pathname === SIGN_UP ? (
+        <BgImageWrapper />
+      ) : undefined}
     </Container>
   )
 }
