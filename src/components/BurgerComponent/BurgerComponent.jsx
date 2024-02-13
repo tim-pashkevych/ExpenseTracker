a8fb8019ce3c9c9f4e65275197285ef965dc2fcc
Zhurbey Artem <zhurbeyartem@gmail.com>
2024-02-13 12:36:20 +0200

    fix

--- src/components/BurgerComponent/BurgerComponent.jsx
+++ src/components/BurgerComponent/BurgerComponent.jsx
@@ -2,16 +2,44 @@ import React, { useState } from "react"
 import styles from "./BurgerComponent.module.css"
 import BurgerIcon from "@/assets/icons/Burger.svg?react"
 import { BurgerMenuModal } from "../BurgerMenuModal/BurgerMenuModal"
+import { Modal } from "../Modal/Modal"
+import { SureLogOutModal } from "../SureLogOutModal/SureLogOutModal"
+import { UserSetsModal } from "../UserSetsModal/UserSetsModal"
 
 export const BurgerComponent = () => {
   const [isModal, setIsModal] = useState(false)
+  const [isVisibleLogout, setIsVisibleLogout] = useState(false)
+  const [isVisibleProfile, setIsVisibleProfile] = useState(false)
   return (
-    <div className={styles.wrapperBurger}>
-      <button className={styles.buttonBurger} onClick={() => setIsModal(true)}>
-        <BurgerIcon className={styles.svgBurger} />
-      </button>
+    <>
+      <div className={styles.wrapperBurger}>
+        <button
+          className={styles.buttonBurger}
+          onClick={() => setIsModal(true)}
+        >
+          <BurgerIcon className={styles.svgBurger} />
+        </button>
 
-      <BurgerMenuModal isOpened={isModal} onClose={() => setIsModal(false)} />
-    </div>
+        <BurgerMenuModal
+          isOpened={isModal}
+          setIsVisibleLogout={setIsVisibleLogout}
+          setIsVisibleProfile={setIsVisibleProfile}
+          onClose={() => setIsModal(false)}
+        />
+      </div>
+
+      <Modal
+        isOpened={isVisibleLogout}
+        onClose={() => setIsVisibleLogout(false)}
+      >
+        <SureLogOutModal closeModal={setIsVisibleLogout} />
+      </Modal>
+      <Modal
+        isOpened={isVisibleProfile}
+        onClose={() => setIsVisibleProfile(false)}
+      >
+        <UserSetsModal closeModal={setIsVisibleProfile} />
+      </Modal>
+    </>
   )
 }
--- src/components/UserBarBtnBox/UserBarBtnBox.jsx
+++ src/components/UserBarBtnBox/UserBarBtnBox.jsx
@@ -6,16 +6,34 @@ import clsx from "clsx"
 import UserBarIcon from "@/assets/icons/UserBarIcon.svg?react"
 import UserBarLogOutIcon from "@/assets/icons/UserBarLogOutIcon.svg?react"
 import DropIcon from "@/assets/icons/DropIcon.svg?react"
+import { Modal } from "../Modal/Modal"
+import { SureLogOutModal } from "../SureLogOutModal/SureLogOutModal"
+import { UserSetsModal } from "../UserSetsModal/UserSetsModal"
 
-export const UserBarBtnBox = () => {
+export const UserBarBtnBox = ({
+  onClose,
+  setIsVisibleProfile,
+  setIsVisibleLogout,
+}) => {
   const [rotated, setrotated] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
+
   const name = useSelector(selectName)
   const avatar = useSelector(selectAvatarUrl)
   const altText = name ? name[0] : ""
   const handleChangeBar = () => {
     setIsOpen(!isOpen)
     setrotated(prev => !prev)
+  }
+
+  const handleProfile = () => {
+    onClose(true)
+    setIsVisibleProfile(true)
+  }
+
+  const handleLogout = () => {
+    setIsVisibleLogout(true)
+    onClose(true)
   }
   return (
     <div className={styles.boxAbsolute}>
@@ -41,13 +59,13 @@ export const UserBarBtnBox = () => {
         </button>
         <ul className={isOpen ? styles.listDrop : styles.listNone}>
           <li className={styles.itemDrop}>
-            <button className={styles.buttonStyle}>
+            <button className={styles.buttonStyle} onClick={handleProfile}>
               <UserBarIcon className={styles.iconUserHower} />
               Profile settings
             </button>
           </li>
           <li className={styles.itemDrop}>
-            <button className={styles.buttonStyle}>
+            <button className={styles.buttonStyle} onClick={handleLogout}>
               <UserBarLogOutIcon className={styles.iconUserHower} />
               Log out
             </button>
