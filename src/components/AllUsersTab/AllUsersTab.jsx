556194f3a5302d09e94c8f2051854223fde21f83
Annazubakha <korepovaanna@gmail.com>
2024-02-13 17:46:11 +0100

    changed path to images @

--- src/components/AllUsersTab/AllUsersTab.jsx
+++ src/components/AllUsersTab/AllUsersTab.jsx
@@ -1,4 +1,4 @@
-import styles from "./AllUsersTab.module.css";
+import styles from "./AllUsersTab.module.css"
 
 export const AllUsersTab = () => {
   return (
@@ -7,48 +7,48 @@ export const AllUsersTab = () => {
         <li className={styles.usersPhotosElem}>
           <picture>
             <source
-              srcSet="
-            ../../../src/assets/images/women1@1x.png 1x,
-            ../../../src/assets/images/women1@2x.png 2x
-          "
-              type="image/png"
+              srcSet='
+            @/assets/images/women1@1x.png 1x,
+            @/assets/images/women1@2x.png 2x
+          '
+              type='image/png'
             />
             <img
               className={styles.userPhoto}
-              src="../../../src/assets/images/women1@1x.png"
-              alt="Photo of the user"
+              src='@/assets/images/women1@1x.png'
+              alt='Photo of the user'
             />
           </picture>
         </li>
         <li className={styles.usersPhotosElemTwo}>
           <picture>
             <source
-              srcSet="
-            ../../../src/assets/images/men@1x.png 1x,
-            ../../../src/assets/images/men@2x.png 2x
-          "
-              type="image/png"
+              srcSet='
+           @/assets/images/men@1x.png 1x,
+            @/assets/images/men@2x.png 2x
+          '
+              type='image/png'
             />
             <img
               className={styles.userPhoto}
-              src="../../../src/assets/images/men@1x.png"
-              alt="Photo of the user"
+              src='@/assets/images/men@1x.png'
+              alt='Photo of the user'
             />
           </picture>
         </li>
         <li>
           <picture>
             <source
-              srcSet="
-            ../../../src/assets/images/women2@1x.png 1x,
-            ../../../src/assets/images/women2@2x.png 2x
-          "
-              type="image/png"
+              srcSet='
+            @/assets/images/women2@1x.png 1x,
+            @/assets/images/women2@2x.png 2x
+          '
+              type='image/png'
             />
             <img
               className={styles.userPhoto}
-              src="../../../src/assets/images/women2@1x.png"
-              alt="Photo of the user"
+              src='@/assets/images/women2@1x.png'
+              alt='Photo of the user'
             />
           </picture>
         </li>
@@ -60,5 +60,5 @@ export const AllUsersTab = () => {
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
